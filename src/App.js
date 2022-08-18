5fdacda8d4c0352bf5d3a6fffa9a3967ba7713ef
tharunoptimus <tharunoptimus@outlook.com>
2022-08-19 04:54:03 +0530

    Replaced the functional component `App` with React Class Component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,70 @@
-import { useEffect, useState } from "react"
+import React from 'react';
 import "./App.css"
 
-function App() {
-	let [setupText, setSetupText] = useState("")
-	let [deliveryText, setDeliveryText] = useState("")
 
-	useEffect(() => {
-		let url =
-			"https://v2.jokeapi.dev/joke/Programming?type=twopart"
-		fetch(url)
-			.then((response) => {
-				return response.json()
+class App extends React.Component {
+	state = {
+		loading: true,
+		data: null
+	}
+
+	async componentDidMount() {
+
+		try {
+			let response = await fetch("https://v2.jokeapi.dev/joke/Programming?type=twopart")
+			let data = await response.json()
+			this.setState({
+				loading: false,
+				data: data
 			})
-			.then((data) => {
-				setSetupText(data.setup)
-				setDeliveryText(data.delivery)
+			
+		} catch (error) {
+			this.setState({
+				loading: false,
+				data: { setup: "Still Loading...", delivery: "Try checking your internet connection 😏" }
 			})
-			.catch((error) => console.log(error))
-	}, [])
+		}
 
-	return (
-		<>
-			<div className="wrapper">
-				<span className="setup">{setupText}</span>
+	}
 
-				<span className="delivery">{deliveryText}</span>
-			</div>
-		</>
-	)
+	render () {
+
+		if(this.state.loading) {
+			return (
+				<>
+					<div className="wrapper">
+						<span className="setup">Loading...</span>
+					</div>
+				</>
+			)
+
+		}
+
+		console.log(this.state.data)
+
+		if(!this.state.data.setup) {
+			return (
+				<>
+					<div className="wrapper">
+						<span className="setup">Unable to load the joke 😔</span>
+					</div>
+				</>
+			)
+		}
+
+		return (
+			<>
+				<div className="wrapper">
+					<span className="setup">{this.state.data.setup}</span>
+	
+					<span className="delivery">{this.state.data.delivery}</span>
+				</div>
+			</>
+		)
+
+
+	}
+
 }
 
 export default App
