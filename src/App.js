5606bb78b01283d20640a4c7b2f8ee6b4eab428f
tharunoptimus <tharunoptimus@outlook.com>
2022-08-19 04:41:02 +0530

    Working fine.. but for some reason fetches twice

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import './App.css';
+import { useEffect, useState } from "react"
+import "./App.css"
 
 function App() {
-  return (
-   <></> 
-  )
+	let [setupText, setSetupText] = useState("")
+	let [deliveryText, setDeliveryText] = useState("")
+
+	useEffect(() => {
+		let url =
+			"https://v2.jokeapi.dev/joke/Programming?type=twopart"
+		fetch(url)
+			.then((response) => {
+				return response.json()
+			})
+			.then((data) => {
+				setSetupText(data.setup)
+				setDeliveryText(data.delivery)
+			})
+			.catch((error) => console.log(error))
+	}, [])
+
+	return (
+		<>
+			<div className="wrapper">
+				<span className="setup">{setupText}</span>
+
+				<span className="delivery">{deliveryText}</span>
+			</div>
+		</>
+	)
 }
 
-export default App;
+export default App
